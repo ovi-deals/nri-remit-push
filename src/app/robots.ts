01844094a882c095961296acbe137a5337c9d73d
ovi-deals <sales@oraa.au>
2026-06-19 17:37:40 +1000

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://nri-remit.vercel.app";
+const BASE_URL = "https://nritransfer.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
